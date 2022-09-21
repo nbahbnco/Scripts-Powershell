aca4d57f93921d49f4825f8656c0a7bdba32b639
Iván <ivan@ivanviso.es>
2022-09-21 13:16:34 +0200

    Limpieza

--- ScanZabbixSender.ps1
+++
@@ -1,3 +0,0 @@
-Get-Childitem -Path C:\ -Recurse -ErrorAction SilentlyContinue -Include *zabbix_sender.exe* -File | ForEach-Object {$_.Fullname | Out-File -Filepath ./ZabbixSenderPath}
-# Crear campo para eventos.
-New-EventLog -LogName Application -Source "Zabbix Sender"
